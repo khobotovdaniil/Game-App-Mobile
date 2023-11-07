af4400b6b4e838e83538a213e0bd5824d7c0ecaa
Daniil_Khobotov <dinich44@gmail.com>
2023-11-07 10:17:57 +0400

    add title to game screen

---
+++ components/Title.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+export const Title = (props) => {
+  return <Text style={styles.title}>{props.children}</Text>
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#ddb52f',
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: '#ddb52f',
+    padding: 12
+  }
+})
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,15 +1,25 @@
 import React from "react"
-import { StyleSheet, View, Text } from "react-native"
+import { StyleSheet, View, Text } from "react-native";
+
+import { Title } from "../components/Title";
 
 export const GameScreen = () => {
 
   return (
-    <View>
-      <Text>Game Screen!</Text>
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <View>
+        <Text>Higher or lower?</Text>
+
+      </View>
+      <Text>Log</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    flex: 1,
+    padding: 24,
+  }
 })
